449f3ee598176b4185607bd24e18f410cda212aa
ablanchard <ablanchard@mirakl.com>
2016-09-15 10:47:11 +0200

    Prevent several starting

--- mopidy_hipchat/start.py
+++ mopidy_hipchat/start.py
@@ -17,16 +17,21 @@ class StartListener(CommandListener):
     def __init__(self,core, config):
         self.core = core
         self.config =config
+        self.started = False
 
     def command(self):
         return '/start'
 
     def action(self, msg):
+        if self.started:
+            return 'Already started'
+
         playlists = self.core.playlists.as_list().get()
         uri = self.find_default_playlist(playlists)
         self.core.tracklist.add(uri=uri)
         self.core.tracklist.shuffle()
         self.core.playback.play()
+        self.started = True
         return 'On air'
 
     def usage(self):
